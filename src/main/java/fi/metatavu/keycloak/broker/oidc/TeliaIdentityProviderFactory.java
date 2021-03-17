b4d37a7e07dba8f14cfe8c847ecfec533421a058
Antti Leppä <antti.leppa@metatavu.fi>
2021-03-17 14:21:54 +0200

    Added identity provider

---
+++ src/main/java/fi/metatavu/keycloak/broker/oidc/TeliaIdentityProviderFactory.java
@@ -0,0 +1,33 @@
+package fi.metatavu.keycloak.broker.oidc;
+
+import org.keycloak.broker.oidc.OIDCIdentityProviderFactory;
+import org.keycloak.models.IdentityProviderModel;
+import org.keycloak.models.KeycloakSession;
+
+/**
+ * OIDC identity provider factory for Telia Tunnistus
+ */
+public class TeliaIdentityProviderFactory extends OIDCIdentityProviderFactory {
+
+	public static final String PROVIDER_ID = "telia";
+
+	@Override
+	public String getName() {
+		return "Telia Tunnistus";
+	}
+
+	@Override
+	public TeliaIdentityProvider create(KeycloakSession session, IdentityProviderModel model) {
+		return new TeliaIdentityProvider(session, new TeliaIdentityProviderConfig(model));
+	}
+
+	@Override
+	public TeliaIdentityProviderConfig createConfig() {
+		return new TeliaIdentityProviderConfig();
+	}
+
+	@Override
+	public String getId() {
+		return PROVIDER_ID;
+	}
+}
